7a468aa4ab3dac74b1a563b023d8f6e7d263bb15
mangohill <54974155+mangohill@users.noreply.github.com>
2026-09-08 14:16:45 +1000

    ADI

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // Scope: https://mangohill.github.io/mango-portracker/
 // ─────────────────────────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'portfolio-tracker-2';
+const CACHE_NAME = 'portfolio-tracker-3';
 const BASE       = '/mango-portracker/';
 
 const PRECACHE_URLS = [
